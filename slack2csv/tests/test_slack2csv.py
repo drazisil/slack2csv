9a9a0f07eb1d0f269fabce56a677c87c91927dd3
Tom Atkins <tom@keybits.net>
2017-12-19 00:05:25 +0000

    change test

--- slack2csv/tests/test_slack2csv.py
+++ slack2csv/tests/test_slack2csv.py
@@ -1,6 +1,6 @@
 # Standard library imports...
 import pytest
-from unittest.mock import Mock, patch
+from mock import Mock, patch
 
 # Local imports...
 from slack2csv.slack2csv import fetch_from_slack, lookup_channel_id_by_name
