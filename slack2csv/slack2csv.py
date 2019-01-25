10fb00d383950ff84c97883afb65cb09f14e78f1
Masen Furer <m_github@0x26.net>
2019-01-25 23:50:39 +0000

    use paged_query generator to paginate through the API results
    ensure that channels and users can be found in large workspaces
    
    refactor message fetching to use paged_query and enumerate through the
    entire response rather than manipulating the date range for each request
    
    fixes the timedelta calculation to use time.mktime for python2.7
    compat

--- slack2csv/slack2csv.py
+++ slack2csv/slack2csv.py
@@ -8,15 +8,35 @@ import requests
 import time
 
 
+QUERY_API_TIMEOUT = 2
+
+
+def paged_query(url):
+    query_url = url
+    next_cursor = True
+    while next_cursor:
+        r = requests.get(query_url)
+        resp = r.json()
+
+        yield resp
+
+        next_cursor = resp.get("response_metadata", {}).get("next_cursor", None)
+        if next_cursor:
+            query_url = url + "&cursor=" + next_cursor
+        else:
+            break
+        time.sleep(QUERY_API_TIMEOUT)
+
+
 def lookup_user_id_by_name(token, user_name):
-    r = requests.get("https://slack.com/api/users.list?token=" +
-                     token)
+    url = "https://slack.com/api/users.list?token=" + token
+    for user_resp in paged_query(url):
 
-    user_list_parsed = r.json()["members"]
+        user_list_parsed = user_resp["members"]
 
-    for user in user_list_parsed:
-        if user["name"] == user_name:
-            return user["id"]
+        for user in user_list_parsed:
+            if user["name"] == user_name:
+                return user["id"]
 
     return ""
 
@@ -25,63 +45,48 @@ def lookup_channel_id_by_name(token, channel_name, types=None):
     url = "https://slack.com/api/conversations.list?token=" + token
     if types is not None:
         url += "&types=" + types
-    query_url = url
+    for channel_resp in paged_query(url):
 
-    channel_list_parsed = [None]
-    while len(channel_list_parsed) > 0:
-        r = requests.get(query_url)
-        resp = r.json()
-
-        channel_list_parsed = resp["channels"]
+        channel_list_parsed = channel_resp["channels"]
 
         for channel in channel_list_parsed:
-            if channel_name in channel.get("name", channel.get("user", None)):
+            if channel_name == channel.get("name", channel.get("user", None)):
                 return channel["id"]
-
-        next_cursor = resp.get("response_metadata", {}).get("next_cursor", None)
-        if next_cursor:
-            query_url = url + "&cursor=" + next_cursor
-        else:
-            break
 
     return ""
 
 
-def fetch_from_slack(token, channel, offset):
-    results = []
-    newest_timestamp = offset
-    more_results = True
-
+def fetch_from_slack(token, channel, oldest):
+    n_messages = 0
+    newest = None
+    oldest = float(oldest)
+    oldest_ts = str(datetime.fromtimestamp(oldest))
     spinner = Spinner('Fetching history for ' +
-                      channel + ' from ' + str(datetime.fromtimestamp(int(offset))) + ' ')
+                      channel + ' from ' + oldest_ts + ' ')
 
-    while more_results == True:
-        print(str(datetime.fromtimestamp(float(newest_timestamp))))
-        r = requests.get("https://slack.com/api/conversations.history?token=" +
-                         token + "&channel=" + channel + "&count=100&inclusive=true&oldest=" + newest_timestamp)
-
-        channel_parsed = r.json()
-
-        if not channel_parsed['ok']:
+    url = ("https://slack.com/api/conversations.history?token=" + token +
+           "&channel=" + channel +
+           "&count=100&inclusive=true&oldest=" + str(round(oldest)))
+    # records are paged oldest to newest, however the message order
+    # within a single response is newest to oldest
+    for message_resp in paged_query(url):
+        if not message_resp['ok']:
             raise ValueError("Error fetching channel history from Slack: ",
-                             channel_parsed["error"])
+                             message_resp["error"])
+        messages = message_resp['messages']
+        newest = messages[0].get('ts', time.time())
+        n_messages += len(messages)
 
-        more_results = channel_parsed["has_more"]
-
-        message_data = channel_parsed['messages']
-
-        # yield interim results
-        for message in message_data:
+        for message in reversed(messages):
             yield message
-
-        newest_timestamp = message_data[0].get('ts')
-
-        time.sleep(2)
         spinner.next()
+    print("\nFetched {0} total messages from {1} to {2}".format(
+                n_messages,
+                oldest_ts,
+                str(datetime.fromtimestamp(float(newest)))))
 
 
 def main():
-
     parser = argparse.ArgumentParser(description='slack2csv')
     parser.add_argument('--text', help='text to search for', default='')
     parser.add_argument(
@@ -129,8 +134,8 @@ def main():
         print("Could not find a valid conversation id. Exiting...")
         return False
 
-    time_diff = str((datetime.now() - timedelta(days=int(args.past_days))
-                     ).timestamp()).split('.')[0]
+    time_diff = time.mktime((datetime.now() -
+                             timedelta(days=int(args.past_days))).timetuple())
 
     # open a file for writing
 
